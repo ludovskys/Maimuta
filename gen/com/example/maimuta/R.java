d11b3a95cc36addab838e75b13e843b2eb6886fb
Ludovic Smoczynski <ludovskys@gmail.com>
2013-05-23 09:53:14 +0200

    ListView qui marche

--- gen/com/example/maimuta/R.java
+++ gen/com/example/maimuta/R.java
@@ -29,23 +29,29 @@ public final class R {
         public static final int custom_rounded_shape=0x7f020000;
         public static final int custom_start_button=0x7f020001;
         public static final int foret=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int start_button_highlighted=0x7f020004;
-        public static final int start_button_normal=0x7f020005;
+        public static final int gradient_bg_hover_listview_parameters=0x7f020003;
+        public static final int gradient_bg_listview_parameters=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int listview_parameters_selector=0x7f020006;
+        public static final int start_button_highlighted=0x7f020007;
+        public static final int start_button_normal=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090008;
         public static final int buttonStartTest=0x7f090005;
+        public static final int duration=0x7f090007;
         public static final int editTextName=0x7f090004;
         public static final int listView1=0x7f090000;
         public static final int listViewTrainingProgramParameters=0x7f090002;
         public static final int relativeLayoutMain=0x7f090003;
         public static final int textView1=0x7f090001;
+        public static final int title=0x7f090006;
     }
     public static final class layout {
         public static final int activity_before_dms_or_dnms_test=0x7f030000;
         public static final int activity_before_training_program_test=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int listview_parameters_row=0x7f030003;
     }
     public static final class menu {
         public static final int before_dmsor_dnm=0x7f080000;
--- src/com/example/maimuta/BeforeTrainingProgramTestActivity.java
+++ src/com/example/maimuta/BeforeTrainingProgramTestActivity.java
@@ -4,26 +4,87 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class BeforeTrainingProgramTestActivity extends Activity {
 	
+	// All static variables
+    static final String URL = "http://api.androidhive.info/music/music.xml";
+    // XML node keys
+    static final String KEY_SONG = "song"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_TITLE = "title";
+    static final String KEY_ARTIST = "artist";
+    static final String KEY_DURATION = "duration";
+    static final String KEY_THUMB_URL = "thumb_url";
+	
 	ListView listViewTrainingProgramParameters;
+	LazyAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_before_training_program_test);
 		
+		Log.d("info","cfffafas");
+		
 		listViewTrainingProgramParameters = (ListView)findViewById(R.id.listViewTrainingProgramParameters);
+		
+		final ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+    	
+        // looping through all song nodes &lt;song&gt;
+        for (int i = 0; i < 5; i++) 
+        {
+            // creating new HashMap
+        	HashMap<String, String> map = new HashMap<String, String>();
+            //Element e = (Element) nl.item(i);
+            // adding each child node to HashMap key =&gt; value
+        	/*
+            map.put(KEY_ID, parser.getValue(e, KEY_ID));
+            map.put(KEY_TITLE, parser.getValue(e, KEY_TITLE));
+            map.put(KEY_ARTIST, parser.getValue(e, KEY_ARTIST));
+            map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
+            map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+            */
+
+            map.put(KEY_TITLE, "Nom paramètre");
+            map.put(KEY_DURATION, "Valeur paramètre");
+
+            // adding HashList to ArrayList
+            songsList.add(map);
+        }
+        
+        // Getting adapter by passing xml data ArrayList
+        adapter=new LazyAdapter(this, songsList);
+        listViewTrainingProgramParameters.setAdapter(adapter);
+ 
+        // Click event for single list row
+        listViewTrainingProgramParameters.setOnItemClickListener(new OnItemClickListener() {
+ 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+            	
+            	TextView duration = (TextView)view.findViewById(R.id.duration); // duration
+            	
+            	duration.setText("Salut les gens j'ai changé lol");
+            }
+        });
 
 	}
 
